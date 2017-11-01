cbb30feead87fa4900351f04823312992ed42c52
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-01 10:49:27 +0530

    removed bottle sections.

--- moose.rb
+++ moose.rb
@@ -5,14 +5,6 @@ class Moose < Formula
   sha256 "a542983e4903b2b51c79f25f4acc372555cbe31bc0b9827302a6430fad466ef7"
   head "https://github.com/BhallaLab/moose-core.git"
 
-  bottle do
-    cellar :any
-    sha256 "6077f886560480c956270f855cf9576a3e8261c5f2ea064117c3483f74a84462" => :sierra
-    sha256 "a637de34ce0b92f16afc120ecb2e0e4aff8f8a2e6a2ada5521ee01cf7ccdca9e" => :el_capitan
-    sha256 "1bb0712ef178577a3c44190be8f21f894cddc66ce03f742d768e44371425dce7" => :yosemite
-    sha256 "a62366e1e1de37c13dec6d2b7f91dc63f8b40ab460e35b31a4d94507a0df6219" => :x86_64_linux
-  end
-
   depends_on "cmake" => :build
   depends_on "gsl"
   depends_on "hdf5"
