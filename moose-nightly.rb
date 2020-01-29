fcd74ef4b60e62813837fb893d5557a52e21a2c3
Dilawar Singh <dilawars@ncbs.res.in>
2020-01-29 10:02:03 +0530

    python2 is EOL. Upgrading to python3. Probably this is the reason behind

--- moose-nightly.rb
+++ moose-nightly.rb
@@ -1,12 +1,12 @@
 class MooseNightly < Formula
   desc "Multiscale Object Oriented Simulation Environment"
   homepage "http://moose.ncbs.res.in"
-  head "https://github.com/BhallaLab/moose-core.git", :branch => "chhennapoda"
+  head "https://github.com/BhallaLab/moose-core.git", :branch => "master"
 
   depends_on "cmake" => :build
   depends_on "gsl"
-  depends_on :python if MacOS.version <= :snow_leopard
   depends_on "numpy"
+  depends_on "python@3" if MacOS.version <= :snow_leopard
 
   def install
     (buildpath/"VERSION").write("#{version}\n")
@@ -15,19 +15,19 @@ class MooseNightly < Formula
     mkdir "_build" do
       system "cmake", "..", *args
       system "make"
-      system "make install"
+      system "make", "install"
     end
   end
 
   def caveats; <<-EOS
     Please also install the following using pip
-      $ pip install matplotlib networkx 
+      $ pip install matplotlib networkx
     Optionally, you can install the folllowing for sbml and NeuroML support.
       $ pip install python-libsbml pyNeuroML
-    EOS
+  EOS
   end
 
   test do
-    system "python", "-c", "import moose;print(moose.__file__)"
+    system "python3", "-c", "import moose;print(moose.__file__, moose.__version__)"
   end
 end
